996c0a00c7f6dc36bc2263cf1a67a7b4c7eb2cf5
shreyasuratran <shreya.suratran@gmail.com>
2024-02-17 22:53:38 -0600

    it works

---
+++ frontend/HomeScreen.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import logoImage from './salud_logo.png';
+import Quiz from './Quiz';
+
+const HomeScreen = ({ navigation }) => {
+    return (
+      <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image source={logoImage} style={styles.logo} />
+        </View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('Quiz')}>
+          <Text style={styles.buttonText}>Take the Quiz</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+  
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#A1E3A1',
+  },
+  logoContainer: {
+    marginBottom: 20,
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+  },
+  button: {
+    backgroundColor: '#ffffff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333333',
+  },
+});
+
+export default HomeScreen;
+
